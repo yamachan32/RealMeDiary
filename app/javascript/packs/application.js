654a96594d05045b336dcfc6895c53d22ee57d2a
jiheekwon <konn3444@gmail.com>
2022-04-01 18:34:58 +0900

    error直し(4)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,9 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 require("../show_images")
 require("../submit_images")
-require('jquery')
-require('jquery_ujs')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/javascript/show_images.js
+++ app/javascript/show_images.js
@@ -1,9 +1,7 @@
-function show_images() {
+window.addEventListener('load', function(){
   $('.slider').slick({
     prevArrow: '<button class="slide-arrow prev-arrow"></button>',
     nextArrow: '<button class="slide-arrow next-arrow"></button>',
     dots: true,
   });
-};
-
-window.addEventListener('load', show_images);
+});
