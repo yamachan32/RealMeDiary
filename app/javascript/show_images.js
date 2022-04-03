3ed6e1d246d97407e47fde3bfea9f9ec3f535c82
jiheekwon <konn3444@gmail.com>
2022-04-03 20:00:38 +0900

    error直し(5)(slick機能)

--- app/javascript/show_images.js
+++ app/javascript/show_images.js
@@ -1,7 +1,9 @@
-window.addEventListener('load', function(){
-  $('.slider').slick({
-    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-    nextArrow: '<button class="slide-arrow next-arrow"></button>',
-    dots: true,
+$(function(){
+  window.addEventListener('load', function(){
+    $('.slider').slick({
+      prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+      nextArrow: '<button class="slide-arrow next-arrow"></button>',
+      dots: true,
+    });
   });
 });
